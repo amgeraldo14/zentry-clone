e25dc5e4a5a3f798cb8f1b1bbc8b3831b239257d
amgeraldo14 <73383622+amgeraldo14@users.noreply.github.com>
2025-01-05 16:08:37 +0700

    about section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <main className="min-h-screen w-screen overflow-x-hidden">
       <HeroSection />
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 };
---
+++ src/components/AnimatedTitle.tsx
@@ -0,0 +1,48 @@
+import { cn } from "../lib/utils";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const AnimatedTitle = ({
+  title,
+  containerClassName,
+}: {
+  title: string;
+  containerClassName: string;
+}) => {
+  useGSAP(() => {
+    const titleAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".animated-title",
+        start: "100 bottom",
+        end: "center bottom",
+        toggleActions: "play none none reverse",
+      },
+    });
+    titleAnimation.to(".animated-word", {
+      opacity: 1,
+      transform: "translate3d(0,0,0) rotateY(0deg) rotateX(0deg)",
+      ease: "power2.inOut",
+      stagger: 0.02,
+    });
+  });
+  return (
+    <div className={cn("animated-title", containerClassName)}>
+      {title.split("<br />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
